356fe18cef51f9886a0cd48ebfaf7ac7e0adadbe
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-13 17:54:11 +0100

    :recycle: refactor(update): Allow to create file if it does not exist.

--- src/lib/update.js
+++ src/lib/update.js
@@ -1,5 +1,16 @@
-export default async function update({read, write, edit}) {
-	const original = await read();
+export default function update(options) {
+	return subroutine({overwrite: true, create: false, ...options});
+}
+
+async function subroutine({read, write, edit, overwrite, create}) {
+	let original;
+	try {
+		original = await read();
+		if (!overwrite) throw new Error('file exists');
+	} catch (error) {
+		if (error.code !== 'ENOENT' || !create) throw error;
+	}
+
 	const edited = await edit(original);
 	await write(edited);
 }
