d0b95536258f3f9ba19643b6eb0d8fee55e495c0
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-15 21:25:17 +0100

    :sparkles: feat: Add transform to define custom script to run on travis.

---
+++ src/transforms/travis:define-custom-script.js
@@ -0,0 +1,54 @@
+import update from '../lib/update.js';
+import replace from '../lib/replace.js';
+
+export const description = 'Define custom script to run on travis.';
+
+export const commit = {
+	subject: description,
+};
+
+const configFile = '.travis.yml';
+const oldScript = 'npm run cover';
+const newScript = 'npm run travis';
+const newScriptDefinition = 'npm run lint && npm run cover';
+
+export async function postcondition({readPkg, readYAML, assert}) {
+	const pkgjson = await readPkg();
+	assert(pkgjson.scripts.travis !== undefined);
+	const config = await readYAML(configFile);
+	assert(!config.script.includes(oldScript));
+	assert(config.script.includes(newScript));
+}
+
+export async function precondition({readPkg, readYAML, assert}) {
+	const pkgjson = await readPkg();
+	assert(pkgjson.scripts.travis === undefined);
+	const config = await readYAML(configFile);
+	assert(config.script.includes(oldScript));
+	assert(!config.script.includes(newScript));
+}
+
+export async function apply({readPkg, writePkg, read, write, fixConfig}) {
+	await update({
+		read: readPkg,
+		write: writePkg,
+		edit: (pkgjson) => {
+			pkgjson.scripts.travis = newScriptDefinition;
+			return pkgjson;
+		},
+	});
+
+	const operations = [[oldScript, () => newScript]];
+	await replace(operations, [configFile], {
+		read,
+		write,
+		method: replace.all,
+	});
+
+	await fixConfig();
+}
+
+export const dependencies = [
+	'package.json:initial-lint',
+	'package.json:setup-xo',
+];
