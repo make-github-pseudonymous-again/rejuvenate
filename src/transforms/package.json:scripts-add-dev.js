07265b33836686457c110ce4a4a1bbcd2bb95954
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-16 11:13:02 +0100

    :sparkles: feat: Add transform that defines dev script.

---
+++ src/transforms/package.json:scripts-add-dev.js
@@ -0,0 +1,36 @@
+import update from '../lib/update.js';
+
+export const description = 'Add dev script.';
+
+export const commit = {
+	subject: description,
+};
+
+export async function postcondition({readPkg, assert}) {
+	const {scripts} = await readPkg();
+	assert(scripts.dev !== undefined);
+}
+
+export async function precondition({readPkg, assert}) {
+	const {scripts} = await readPkg();
+	assert(scripts.dev === undefined);
+	assert(scripts.test.startsWith('ava'));
+	assert(scripts['lint-and-fix'] !== undefined);
+	assert(scripts['lint-config-and-fix'] !== undefined);
+	assert(scripts.cover !== undefined);
+}
+
+export async function apply({readPkg, writePkg, fixConfig}) {
+	await update({
+		read: readPkg,
+		write: writePkg,
+		edit: (pkgjson) => {
+			pkgjson.scripts.dev =
+				'npm run lint-config-and-fix && npm run lint-and-fix && npm run cover -- -- -st --fail-fast';
+			return pkgjson;
+		},
+	});
+	await fixConfig();
+}
+
+export const dependencies = ['config:lint-setup', 'sources:lint-setup'];
