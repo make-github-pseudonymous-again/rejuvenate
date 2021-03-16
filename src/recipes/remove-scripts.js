6e721e34de5166fff29586b9886b22a6d21fd33d
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-16 21:54:26 +0100

    :construction: progress: Add recipe to easily remove scripts.

---
+++ src/recipes/remove-scripts.js
@@ -0,0 +1,63 @@
+import * as pkg from '../lib/pkg.js';
+import update from '../lib/update.js';
+
+const removeScripts = (options) =>
+	subroutine({scripts: [], deps: [], files: [], config: [], ...options});
+
+export default removeScripts;
+
+function subroutine({scripts, deps, files, config}) {
+	return {
+		postcondition: async ({readPkg, exists, assert}) => {
+			const pkgjson = await readPkg();
+			const devDeps = pkg.devDeps(pkgjson);
+			for (const dep of deps) assert(!devDeps.has(dep));
+			for (const key of scripts) {
+				assert(pkgjson.scripts[key] === undefined);
+			}
+
+			for (const key of config) {
+				assert(pkgjson[key] === undefined);
+			}
+
+			for (const filename of files) {
+				// eslint-disable-next-line no-await-in-loop
+				assert(!(await exists(filename)));
+			}
+		},
+
+		precondition: async ({readPkg, exists, assert}) => {
+			const pkgjson = await readPkg();
+			const devDeps = pkg.devDeps(pkgjson);
+			for (const dep of deps) assert(devDeps.has(dep));
+			for (const key of scripts) {
+				assert(pkgjson.scripts[key] !== undefined);
+			}
+
+			for (const key of config) {
+				assert(pkgjson[key] !== undefined);
+			}
+
+			for (const filename of files) {
+				// eslint-disable-next-line no-await-in-loop
+				assert(await exists(filename));
+			}
+		},
+
+		apply: async ({readPkg, writePkg, remove, fixConfig, install}) => {
+			await update({
+				read: readPkg,
+				write: writePkg,
+				edit: (pkgjson) => {
+					for (const dep of deps) delete pkgjson.devDependencies[dep];
+					for (const key of scripts) delete pkgjson.scripts[key];
+					for (const key of config) delete pkgjson[key];
+					return pkgjson;
+				},
+			});
+			await remove(files);
+			await fixConfig();
+			await install();
+		},
+	};
+}
