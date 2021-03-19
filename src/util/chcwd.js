7d512ca72815989acfc8ac694bd4e6c41bb45099
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-19 11:04:26 +0100

    :adhesive_bandage: fix(chcwd): Add trailing newline to YAML files.

--- src/util/chcwd.js
+++ src/util/chcwd.js
@@ -69,7 +69,8 @@ export default function chcwd({
 	const writeJSON = (path, data) =>
 		_writeJsonFile(resolve(cwd, path), data, {detectIndent: true});
 	const readYAML = async (path) => parseYAML(await read(path));
-	const writeYAML = async (path, data) => write(path, stringifyYAML(data));
+	const writeYAML = async (path, data) =>
+		write(path, stringifyYAML(data) + '\n');
 	const readPkg = () => readJSON('package.json');
 	const writePkg = (data) => writeJSON('package.json', data);
 	const glob = (patterns, options) => fg.stream(patterns, {...options, cwd});
