c49b495bb7cf0f1290b0f09fc31eab036a3409b2
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-19 18:23:16 +0100

    :sparkles: feat: Add transforms to fix .gitignore.

---
+++ src/transforms/gitignore:fix-gh-pages.js
@@ -0,0 +1,30 @@
+import replace from '../lib/lines/replace.js';
+import find from '../lib/lines/find.js';
+
+const filename = '.gitignore';
+const directory = 'gh-pages';
+const pattern = /^gh-pages/;
+export const description = `Add leading slash to ignored ${directory} directory.`;
+
+export const commit = {
+	scope: filename,
+	subject: description,
+};
+
+export async function postcondition({lines, assert}) {
+	const found = await find([pattern], [filename], {lines, method: find.regexp});
+	assert(!found);
+}
+
+export async function precondition({lines, assert}) {
+	const found = await find([pattern], [filename], {lines, method: find.regexp});
+	assert(found);
+}
+
+export async function apply({lines, write}) {
+	await replace([[pattern, '/' + directory]], [filename], {
+		lines,
+		write,
+		method: replace.first,
+	});
+}
