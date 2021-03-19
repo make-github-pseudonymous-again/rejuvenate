d7412224b2511276f00b13104fa15a93c8a63208
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-19 12:00:45 +0100

    :adhesive_bandage: fix(lib/code/lib#is): Make it work when node is undefined.

--- src/lib/code/lib.js
+++ src/lib/code/lib.js
@@ -2,7 +2,7 @@ import {types} from 'recast';
 const n = types.namedTypes;
 const b = types.builders;
 
-const is = (node, type) => node.type === type.name;
+const is = (node, type) => node?.type === type.name;
 
 const lib = {
 	is,
