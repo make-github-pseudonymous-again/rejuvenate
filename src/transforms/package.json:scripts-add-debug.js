1fe650dc2407c6c6395828a28777fe8137a3bd2f
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-05-01 11:19:23 +0200

    :adhesive_bandage: fix(scripts-add-debug): Depend on refactored babel config.

--- src/transforms/package.json:scripts-add-debug.js
+++ src/transforms/package.json:scripts-add-debug.js
@@ -11,7 +11,7 @@ const commit = {
 	subject: description,
 };
 
-const dependencies = ['config:lint-setup', 'babel:setup-env-debug'];
+const dependencies = ['config:lint-setup', 'babel:refactor-config'];
 
 const {postcondition, precondition, apply} = addScripts({scripts});
 
