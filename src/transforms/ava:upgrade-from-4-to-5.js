1f1286f834e1c35580dc19675b0eb3fe7e9016f2
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2024-02-01 21:42:20 +0100

    :sparkles: feat: Add transform to upgrade AVA to v5.

---
+++ src/transforms/ava:upgrade-from-4-to-5.js
@@ -0,0 +1,36 @@
+import update from '../lib/update.js';
+
+export const description = 'Upgrade AVA to v5.';
+
+export const commit = {
+	type: 'deps',
+	subject: description,
+};
+
+export async function postcondition({readPkg, assert}) {
+	const {ava, devDependencies} = await readPkg();
+	assert(devDependencies.ava?.startsWith('5.'));
+	assert(ava?.require?.includes('@babel/register'));
+}
+
+export async function precondition({readPkg, assert}) {
+	const {ava, devDependencies} = await readPkg();
+	assert(devDependencies.ava?.startsWith('4.'));
+	assert(ava?.require?.includes('@babel/register'));
+}
+
+export async function apply({readPkg, writePkg, fixConfig, upgrade, install}) {
+	await update({
+		read: readPkg,
+		write: writePkg,
+		edit(pkgjson) {
+			pkgjson.devDependencies.ava = '5.0.0';
+			return pkgjson;
+		},
+	});
+	await fixConfig();
+	await upgrade('ava', {target: 'minor'});
+	await install();
+}
+
+export const dependencies = ['ava:upgrade-from-3-to-4'];
